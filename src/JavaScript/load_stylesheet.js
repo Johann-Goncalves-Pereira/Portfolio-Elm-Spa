39899cc797006060c3a6b7b1370b8fc166850c4e
Johann Gonçalves Pereira <johann.gon.pereira@gmail.com>
2021-10-22 22:00:58 -0300

    feat: better loaders Scss

--- src/JavaScript/load_stylesheet.js
+++ src/JavaScript/load_stylesheet.js
@@ -2,5 +2,4 @@ function requireAll(r) {
   r.keys().forEach(r);
 }
 
-requireAll(require.context("../Stylesheet", true, /.*/));
-requireAll(require.context("../Stylesheet", true, /.*/));
+requireAll(require.context("../Stylesheet", true, /\.*/));
