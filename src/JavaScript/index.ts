20c124f3de92829d31448ad29e2de443ba383cc7
Johann Gonçalves Pereira <johann.gon.pereira@gmail.com>
2021-10-22 21:56:47 -0300

    feat: take storage and root div
    trying to take the storage data and add a root div

--- src/JavaScript/index.ts
+++ src/JavaScript/index.ts
@@ -2,5 +2,11 @@ const { Elm } = require("../Main.elm");
 import "./load_stylesheet";
 
 let elmApp = Elm.Main.init({
-  node: document.getRootNode(),
+  node: document.getElementById("root"),
+  flags: JSON.parse(localStorage.getItem("storage")),
+});
+
+elmApp.ports.save.subscribe((storage) => {
+  localStorage.setItem("storage", JSON.stringify(storage));
+  elmApp.ports.load.send(storage);
 });
