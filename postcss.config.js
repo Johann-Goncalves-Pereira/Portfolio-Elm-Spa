efc8f066b81dd92dcae16b1b75ad50718af1cb01
Johann Gonçalves Pereira <johann.gon.pereira@gmail.com>
2022-02-21 09:34:07 -0300

    PostCss - Add custom media

--- postcss.config.js
+++ postcss.config.js
@@ -1,10 +1,16 @@
-const postcssJitProps = require("postcss-jit-props");
+// const postcss = require("postcss");
+const doiuse = require("doiuse");
+const autoprefixer = require("autoprefixer");
+const postcssCustomMedia = require("postcss-custom-media");
+
 const OpenProps = require("open-props");
+const postcssJitProps = require("postcss-jit-props");
 
 module.exports = {
   plugins: [
-    require("autoprefixer"),
-    require("doiuse"),
+    autoprefixer,
+    doiuse,
+    postcssCustomMedia(),
     postcssJitProps(OpenProps),
   ],
 };
